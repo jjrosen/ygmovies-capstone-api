4624792b6f5e0dad17973bc5993d2e4d2f059cc6
jjrosen <joshrosen281@gmail.com>
2026-01-19 21:25:37 -0600

    create crud actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,11 +10,41 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @movie = Movie.create(
+      name: params[:name],
+      movie_url: params[:movie_url],
+      poster: params[:poster],
+      overview: params[:overview],
+      actors: params[:actors],
+      rating: params[:rating],
+      user_rating: params[:user_rating],
+      release_date: params[:release_date],
+      genre: params[:genre],
+      run_time: params[:run_time]
+    )
+    render :show
   end
 
   def update
+    @movie = Movie.find_by(id: params[:id])
+    @movie.update(
+        name: params[:name] || @movie.name,
+      movie_url: params[:movie_url] || @movie.movie_url,
+      poster: params[:poster] || @movie.poster,
+      overview: params[:overview] || @movie.overview,
+      actors: params[:actors] || @movie.actors,
+      rating: params[:rating] || @movie.rating,
+      user_rating: params[:user_rating] || @movie.user_rating,
+      release_date: params[:release_date] || @movie.release_date,
+      genre: params[:genre] || @movie.genre,
+      run_time: params[:run_time] || @movie.run_time
+    )
+    render :show
   end
 
   def destroy
+    @movie = Movie.find_by(id: params[:id])
+    @movie.destroy
+    render json: {message: "Movie was Removed Successfully"}
   end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -8,4 +8,23 @@ test "index" do
   data = JSON.parse(response.body)
   assert_equal Movie.count, data.length
 end
+
+test "show" do 
+  
+end
+
+test "create" do 
+  assert_difference "Movie.count", 1 do
+    post "/movies.json", params: {name: "Avengers", movie_url: "Avenger.mp4", poster: "avengers.jpg", overview: "superhero movie", actors: "RDJ, Chris Evans", rating: 8, user_rating: 8, release_date: "2012", genre: "action", run_time: "2 hrs"}
+    assert_response 200
+end
+
+test "update" do 
+
+end
+
+test "destroy" do 
+
+end
+
 end
