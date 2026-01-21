f330588bd91a5a2123586510a2b0fb18df2390ec
jjrosen <joshrosen281@gmail.com>
2026-01-20 19:12:30 -0600

    fix up the schema and craete model for the movie and genres to go through

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_20_021142) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_21_011111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movie_genres", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "genre_id"
+    t.integer "movie_id"
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "actors"
     t.datetime "created_at", null: false
-    t.string "genre"
     t.string "movie_url"
     t.string "name"
     t.string "overview"
